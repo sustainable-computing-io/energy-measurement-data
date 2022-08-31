86170e2ee584c1e3b051513b2c753af5903bf0d1
Huamin Chen <hchen@redhat.com>
2022-08-31 13:14:44 -0500

    update test script

--- energy-instruction/tune-freq.sh
+++ energy-instruction/tune-freq.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
+# pre-requisite
+# sysbench: yum install https://repo.percona.com/yum/release/8/RPMS/x86_64/sysbench-1.0.20-6.el8.x86_64.rpm
 SLEEP=30
 RUNTIME=120
 TOTAL=`expr ${SLEEP} + ${RUNTIME}`
+
+STEP=5
+
+NUM_CPU=$(cat /proc/cpuinfo |grep processor |awk '{print $3}'|sort -g|tail -1)
+CPU_STEP=`expr ${NUM_CPU} / ${STEP}`
+
+FREQ_LOW=$(cpupower frequency-info -l|tail -1|awk '{print $1}')
+FREQ_HIGH=$(cpupower frequency-info -l|tail -1|awk '{print $2}')
+FREQ_RANGE=`expr ${FREQ_HIGH} - ${FREQ_LOW} `
+FREQ_STEP=`expr ${FREQ_RANGE} / ${STEP}`
+FREQ_UNIT="MHz"
 
 function run_test () {
     cpupower set -b 0
@@ -24,15 +37,17 @@ cpupower frequency-set -g performance 2>&1 > /dev/null
 mkdir -p result
 export IP=$(kubectl get svc -n monitoring prometheus-k8s -ojsonpath='{.spec.clusterIP}')
 
-for THREADS in 8 16 32
+for THREADS in $(seq ${CPU_STEP} ${CPU_STEP} ${NUM_CPU})
 do
-    for FREQ in "800MHZ" "1000MHz" "1500MHz" "2000MHz" "2500MHz"
+    for II in $(seq 0 ${STEP})
     do
+        FREQ=`expr ${FREQ_LOW} + ${II} \* ${FREQ_STEP}`
+        echo "THREADS ${THREADS}, FREQ ${FREQ}"
         run_test
         power_saving
         curl "http://${IP}:9090/api/v1/query?query=node_energy_stat[${TOTAL}s]" | jq .data.result[].metric > result/freq-test-${THREADS}-${FREQ}.json
-        cat result/freq-test-${THREADS}-${FREQ}.json | jq -r '[(.curr_energy_in_core|tonumber) / (.curr_cpu_instructions|tonumber)]' |grep -e [0-9] > result/energy_per_instr-${THREADS}-${FREQ}.dat
-        cat result/freq-test-${THREADS}-${FREQ}.json | jq -r '[(.curr_energy_in_core|tonumber)]' |grep -e [0-9] > result/energy-${THREADS}-${FREQ}.dat
-        cat result/freq-test-${THREADS}-${FREQ}.json | jq -r '[(.curr_cpu_instructions|tonumber)]' |grep -e [0-9] > result/instruction-${THREADS}-${FREQ}.dat
+        cat result/freq-test-${THREADS}-${FREQ}.json | jq -r '[(.node_curr_energy_in_core|tonumber) / (.curr_cpu_instructions|tonumber)]' |grep -e [0-9] > result/energy_per_instr-${THREADS}-${FREQ}.dat
+        cat result/freq-test-${THREADS}-${FREQ}.json | jq -r '[(.node_curr_energy_in_core|tonumber)]' |grep -e [0-9] > result/energy-${THREADS}-${FREQ}.dat
+        cat result/freq-test-${THREADS}-${FREQ}.json | jq -r '[(.node_curr_cpu_instructions|tonumber)]' |grep -e [0-9] > result/instruction-${THREADS}-${FREQ}.dat
     done    
 done
